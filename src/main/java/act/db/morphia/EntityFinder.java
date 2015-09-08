c707dfb5848660df59a20c6c34cd4e1dbcea51ef
Gelin Luo <greenlaw110@gmail.com>
2015-09-08 15:01:37 +1000

    workaround of class detector issue

--- src/main/java/act/db/morphia/EntityFinder.java
+++ src/main/java/act/db/morphia/EntityFinder.java
@@ -8,16 +8,23 @@ import org.mongodb.morphia.annotations.Entity;
 import org.osgl._;
 import org.osgl.exception.NotAppliedException;
 
+import java.lang.reflect.Modifier;
 import java.util.Map;
 import java.util.Set;
 
 @ActComponent
 public class EntityFinder extends AnnotatedTypeFinder {
     public EntityFinder() {
-        super(true, false, Entity.class, new _.F2<App, String, Map<Class<? extends AppByteCodeScanner>, Set<String>>>() {
+        super(true, true, Entity.class, new _.F2<App, String, Map<Class<? extends AppByteCodeScanner>, Set<String>>>() {
             @Override
             public Map<Class<? extends AppByteCodeScanner>, Set<String>> apply(App app, String className) throws NotAppliedException, _.Break {
-                MorphiaService.morphia().map(_.classForName(className, app.classLoader()));
+                // TODO Fix me: different class detecting filter shall have different result thus we do not
+                // need the following workarounds
+                Class<?> c = _.classForName(className, app.classLoader());
+                if (Modifier.isAbstract(c.getModifiers())) {
+                    return null;
+                }
+                MorphiaService.morphia().map(c);
                 return null;
             }
         });
