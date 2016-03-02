bdbb0406880fb6a1eac333eaa590f0a319bc6bfa
Gelin Luo <greenlaw110@gmail.com>
2016-03-03 08:13:06 +1100

    add findById(String) method to MorphiaDao

--- src/main/java/act/db/morphia/MorphiaDao.java
+++ src/main/java/act/db/morphia/MorphiaDao.java
@@ -3,6 +3,10 @@ package act.db.morphia;
 import act.util.General;
 import org.bson.types.ObjectId;
 import org.mongodb.morphia.Datastore;
+import org.osgl.Osgl;
+import org.osgl.util.C;
+
+import java.util.Collection;
 
 /**
  * The morphia dao base implementation use {@link org.bson.types.ObjectId} as the ID type
@@ -20,4 +24,21 @@ public class MorphiaDao<MODEL_TYPE>
     }
 
     public MorphiaDao() {}
+
+    public MODEL_TYPE findById(String id) {
+        return findById(new ObjectId(id));
+    }
+
+    public void deleteById(String id) {
+        super.deleteById(new ObjectId(id));
+    }
+
+    public Iterable<MODEL_TYPE> findByIdList(Collection<String> idList) {
+        return super.findByIdList(C.list(idList).map(new Osgl.Transformer<String, ObjectId>() {
+            @Override
+            public ObjectId transform(String s) {
+                return new ObjectId(s);
+            }
+        }));
+    }
 }
