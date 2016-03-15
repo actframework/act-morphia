aa9df3a8d91a6fd9dcdda64c5bed08b8e773ddac
Gelin Luo <greenlaw110@gmail.com>
2016-03-15 20:12:29 +1100

    catch up actframework updates: SubTypeFinder2 renamed to SubTypeFinder

--- src/main/java/act/db/morphia/TypeConverterFinder.java
+++ src/main/java/act/db/morphia/TypeConverterFinder.java
@@ -2,22 +2,13 @@ package act.db.morphia;
 
 import act.ActComponent;
 import act.app.App;
-import act.app.AppByteCodeScanner;
 import act.app.event.AppEventId;
-import act.util.AnnotatedTypeFinder;
-import act.util.SubTypeFinder2;
+import act.util.SubTypeFinder;
 import org.mongodb.morphia.Morphia;
-import org.mongodb.morphia.annotations.Entity;
-import org.mongodb.morphia.converters.SimpleValueConverter;
 import org.mongodb.morphia.converters.TypeConverter;
-import org.osgl.$;
-import org.osgl.exception.NotAppliedException;
-
-import java.util.Map;
-import java.util.Set;
 
 @ActComponent
-public class TypeConverterFinder extends SubTypeFinder2<TypeConverter> {
+public class TypeConverterFinder extends SubTypeFinder<TypeConverter> {
 
     private static final String SYS_CONVERTER_PKG = Morphia.class.getPackage().getName();
 
@@ -30,7 +21,7 @@ public class TypeConverterFinder extends SubTypeFinder2<TypeConverter> {
         if (target.getName().startsWith(SYS_CONVERTER_PKG)) {
             return;
         }
-        app.jobManager().on(AppEventId.DEPENDENCY_INJECTOR_LOADED, new Runnable() {
+        app.jobManager().on(AppEventId.DEPENDENCY_INJECTOR_PROVISIONED, new Runnable() {
             @Override
             public void run() {
                 MorphiaService.mapper().getConverters().addConverter(app.newInstance(target));
