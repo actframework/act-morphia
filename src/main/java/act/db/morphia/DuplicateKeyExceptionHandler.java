011f34634150b43079bd8c43dbb81cb0635fb5e8
Gelin Luo <greenlaw110@gmail.com>
2016-05-07 23:03:25 +1000

    Map duplicateKeyException to Conflict result

---
+++ src/main/java/act/db/morphia/DuplicateKeyExceptionHandler.java
@@ -0,0 +1,26 @@
+package act.db.morphia;
+
+import act.ActComponent;
+import act.app.ActionContext;
+import act.handler.builtin.controller.ActionHandlerInvoker;
+import act.handler.builtin.controller.ExceptionInterceptor;
+import act.view.ActConflict;
+import com.mongodb.DuplicateKeyException;
+import org.osgl.mvc.result.Result;
+
+@ActComponent
+public class DuplicateKeyExceptionHandler extends ExceptionInterceptor {
+    public DuplicateKeyExceptionHandler() {
+        super(0, DuplicateKeyException.class);
+    }
+
+    @Override
+    public void accept(ActionHandlerInvoker.Visitor visitor) {
+        // do nothing
+    }
+
+    @Override
+    protected Result internalHandle(Exception e, ActionContext actionContext) throws Exception {
+        return ActConflict.create(e.getMessage());
+    }
+}
