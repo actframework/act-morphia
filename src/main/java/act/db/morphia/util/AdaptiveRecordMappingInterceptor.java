4dd5e541a8c7faa6152393885895220e69b54912
Gelin Luo <greenlaw110@gmail.com>
2017-03-03 18:55:26 +1100

    fix issue: it load mapped properties into adaptive record KV store

--- src/main/java/act/db/morphia/util/AdaptiveRecordMappingInterceptor.java
+++ src/main/java/act/db/morphia/util/AdaptiveRecordMappingInterceptor.java
@@ -4,16 +4,27 @@ import act.db.AdaptiveRecord;
 import com.alibaba.fastjson.JSONObject;
 import com.mongodb.DBObject;
 import org.mongodb.morphia.AbstractEntityInterceptor;
+import org.mongodb.morphia.annotations.Property;
 import org.mongodb.morphia.mapping.Mapper;
 import org.mongodb.morphia.utils.IterHelper;
 import org.osgl.$;
 import org.osgl.util.C;
 import org.osgl.util.S;
 
+import javax.inject.Singleton;
+import java.lang.reflect.Field;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
+@Singleton
 public class AdaptiveRecordMappingInterceptor extends AbstractEntityInterceptor {
+
+    private final Set<String> BUILT_IN_PROPS = C.setOf("_id,className,_created,_modified,v".split(","));
+    private final ConcurrentMap<Class<?>, Set<String>> mappedPropertiesLookup = new ConcurrentHashMap<>();
 
     @Override
     public void prePersist(Object ent, DBObject dbObj, Mapper mapper) {
@@ -30,11 +41,9 @@ public class AdaptiveRecordMappingInterceptor extends AbstractEntityInterceptor 
         }
     }
 
-    private static final Set<String> BUILT_IN_PROPS = C.setOf("_id,className,_created,_modified,v".split(","));
-
     @Override
     public void postLoad(Object ent, DBObject dbObj, Mapper mapper) {
-        Class<?> c = ent.getClass();
+        final Class<?> c = ent.getClass();
         if (AdaptiveRecord.class.isAssignableFrom(c)) {
             AdaptiveRecord ar = $.cast(ent);
             final Map<String, Object> kv = ar.internalMap();
@@ -43,12 +52,33 @@ public class AdaptiveRecordMappingInterceptor extends AbstractEntityInterceptor 
                 @Override
                 public void eval(final Object k, final Object val) {
                     final String key = S.string(k);
-                    if (BUILT_IN_PROPS.contains(key) || metaInfo.setterFieldSpecs.containsKey(key)) {
+                    if (BUILT_IN_PROPS.contains(key) || metaInfo.setterFieldSpecs.containsKey(key) || mappedProperties(c).contains(key)) {
                         return;
                     }
                     kv.put(key, JSONObject.toJSON(val));
                 }
             });
         }
+    }
+
+    private Set<String> mappedProperties(Class c) {
+        Set<String> set = mappedPropertiesLookup.get(c);
+        if (null == set) {
+            set = findMappedProperties(c);
+            mappedPropertiesLookup.putIfAbsent(c, set);
+        }
+        return set;
+    }
+
+    private Set<String> findMappedProperties(Class c) {
+        Set<String> set = new HashSet<>();
+        List<Field> fieldList = $.fieldsOf(c, true);
+        for (Field field : fieldList) {
+            Property property = field.getAnnotation(Property.class);
+            if (null != property) {
+                set.add(property.value());
+            }
+        }
+        return set;
     }
 }
